5cceb778fb29c46424c10bc80302a7442508df8b
Ben <hostellerie.org@gmail.com>
2026-09-20 15:46:39 +0700

    Restore known-good MediaGallery admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -122,26 +122,25 @@ if ($mode == 'editsubmission') {
 $adminTemplate = new Template($_MG_CONF['template_path']);
 $adminTemplate->set_file('admin_index', 'admin_index.thtml');
 $adminTemplate->set_var(array(
+    'admin_menu'              => MG_showAdminMenu($sub_menu),
     'configuration_url'       => $_CONF['site_admin_url'] . '/configuration.php',
     'help_url'                => $_MG_CONF['admin_url'] . 'help.php',
     'albums_url'              => $_MG_CONF['site_url'] . '/admin.php',
     'member_albums_url'       => $_MG_CONF['admin_url'] . 'index.php?s=m',
     'maintenance_url'         => $_MG_CONF['admin_url'] . 'index.php?s=b',
-    'tools_url'               => $_MG_CONF['admin_url'] . 'index.php?s=c',
-    'categories_url'          => $_MG_CONF['admin_url'] . 'category.php',
+    'misc_url'                => $_MG_CONF['admin_url'] . 'index.php?s=c',
     'lang_configuration'      => $LANG_MG01['configuration'],
     'lang_help'               => $LANG_MG01['help'],
+    'lang_overview'           => $LANG_MG01['overview'],
     'admin_help'              => $LANG_MG01['admin_help'],
-    'lang_albums'             => $LANG_MG01['manage_albums'],
-    'lang_albums_help'        => $LANG_MG01['manage_albums_help'],
+    'lang_albums'             => $LANG_MG01['albums'],
+    'lang_albums_help'        => $LANG_MG01['content_categories_help'],
     'lang_member_albums'      => $LANG_MG01['member_albums'],
     'lang_member_albums_help' => $LANG_MG01['member_albums_help'],
-    'lang_maintenance'        => $LANG_MG01['maintenance_tools'],
-    'lang_maintenance_help'   => $LANG_MG01['maintenance_tools_help'],
-    'lang_tools'              => $LANG_MG01['reports_tools'],
-    'lang_tools_help'         => $LANG_MG01['reports_tools_help'],
-    'lang_categories'         => $LANG_MG01['content_categories'],
-    'lang_categories_help'    => $LANG_MG01['content_categories_help'],
+    'lang_batch_sessions'     => $LANG_MG01['batch_sessions'],
+    'lang_batch_sessions_help'=> $LANG_MG01['maintenance_tools_help'],
+    'lang_miscellaneous'      => $LANG_MG01['miscellaneous'],
+    'lang_misc_help'          => $LANG_MG01['reports_tools_help'],
     'status_message'          => ($msg > 0 && isset($LANG_MG09[$msg]))
         ? COM_showMessageText($LANG_MG09[$msg])
         : ''
