ed2d6cbd649f7d6c23fcdfce59314e5fd2a287d3
Ben <hostellerie.org@gmail.com>
2026-09-20 16:48:23 +0700

    Use shared MediaGallery admin document styling

--- admin/purgealbums.php
+++ admin/purgealbums.php
@@ -197,7 +197,7 @@ $display = COM_startBlock($LANG_MG00['admin'], '', COM_getBlockTemplate('_admin_
 $display .= MG_showAdminMenu('member_albums');
 $display .= $T->finish($T->parse('output', 'admin'));
 $display .= COM_endBlock(COM_getBlockTemplate('_admin_block', 'footer'));
-$display = COM_createHTMLDocument($display);
+$display = MG_adminCreateHTMLDocument($display);
 
 COM_output($display);
 ?>
