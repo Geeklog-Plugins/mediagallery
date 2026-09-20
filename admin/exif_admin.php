5d131ba4b1f600ce15fb9a3b52cf4296543e6ee6
Ben <hostellerie.org@gmail.com>
2026-09-20 16:48:03 +0700

    Use shared MediaGallery admin document styling

--- admin/exif_admin.php
+++ admin/exif_admin.php
@@ -158,7 +158,7 @@ $display = COM_startBlock($LANG_MG00['admin'], '', COM_getBlockTemplate('_admin_
 $display .= MG_showAdminMenu('miscellaneous');
 $display .= $T->finish($T->get_var('output'));
 $display .= COM_endBlock(COM_getBlockTemplate('_admin_block', 'footer'));
-$display = COM_createHTMLDocument($display);
+$display = MG_adminCreateHTMLDocument($display);
 
 COM_output($display);
 ?>
